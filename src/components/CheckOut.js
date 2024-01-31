2f5a688e39e23f242f0613c219456bde4c4bdd80
majidgeeks <majidrajput246@gmail.com>
2024-01-31 16:36:47 +0500

    some changes

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -1,14 +1,44 @@
 import DrawerAppBar from "./AppBar";
-import * as React from 'react';
+import React, {useState, useContext} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
+import CartContext from "../context/cart";
+import axios from "axios";
 
 
 
 
 
 const CheckOut = () => {
+  
+  const[name, setName] = useState();
+  const[address, setAddress] = useState();
+  const[email, setEmail] = useState();
+  const[contact, setContact] = useState();
+
+  const {cart} = useContext(CartContext);
+  
+
+  const placeOrder = () => {
+
+    const user = {
+      name,
+      address,
+      email,
+      contact
+    }
+
+    axios.post("http://localhost:3000/order",{
+      user,
+      cart
+    })
+    .then((res)=>console.log(res))
+    .catch((err)=>console.log(err))
+  }
+
+
+
     return(
 
         <div>
@@ -28,32 +58,32 @@ const CheckOut = () => {
       autoComplete="off"
     >
 
-<TextField className=''
+<TextField value={name} onChange={(e)=> setName(e.target.value)} className=''
           required
           id="outlined-required"
           label="Full Name"
-          defaultValue="Hello World"
+          
         />
-        <TextField
+        <TextField value={address} onChange={(e)=> setAddress(e.target.value)}
           required
           id="outlined-required"
           label="Address"
-          defaultValue="Hello World"
+          
         />
-        <TextField
+        <TextField value={email} onChange={(e)=> setEmail(e.target.value)}
           required
           id="outlined-required"
           label="Email"
-          defaultValue="Hello World"
+          
         />
-        <TextField
+        <TextField value={contact} onChange={(e)=> setContact(e.target.value)}
           required
           id="outlined-required"
           label="Contact"  
-          defaultValue="Hello World"
+        
         />
 
-<Button  style={{backgroundColor:'#8bc34a', color:"#fff", width:'51%', marginTop:10, height:30 }} size='medium' >PLACE ORDER</Button>
+<Button onClick={placeOrder}  style={{backgroundColor:'#8bc34a', color:"#fff", width:'51%', marginTop:10, height:30 }} size='medium' >PLACE ORDER</Button>
     </Box>
 
             </div>
